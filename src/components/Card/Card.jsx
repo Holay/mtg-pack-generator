a5b0ecf43ed0fe68db3a1df848aa16f7c2f78465
Mateo Penagos <mateofer_2@hotmail.com>
2020-01-12 04:52:04 -0500

    added support for dual cards in showcase by clicking to switch faces

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Card.css"
 
 const Card = ({card}) => {
+    const dual = card.image_uris? false:true;
+    const [currentFace, setCurrentFace] = useState(dual?card.card_faces[0].image_uris.normal:[])
+    function handleFlip(){
+        if(dual){
+            setCurrentFace(card.card_faces.find(face=>face.image_uris.normal !== currentFace).image_uris.normal)
+        }
+    }
     return (
         <div className="card">
-            <img className="card-image" src={card.image_uris.normal} alt={card.name} />
+            <img className="card-image" onClick={handleFlip} src={dual ? currentFace:card.image_uris.normal} alt={card.name} />
             <p className="card-name">{card.name}</p>
         </div>
     )

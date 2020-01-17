87ddceea4b407df976ae60036b1661f42f7dca12
Mateo Penagos <mateofer_2@hotmail.com>
2020-01-17 00:29:24 -0500

    removed MTG logo and replaced it with cycling backgrounds

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './mtg-logo.png';
 import './App.css';
-import { SetList, Showcase, StickyNav, Footer} from "./components"
+import { SetList, Showcase, StickyNav, Footer } from "./components"
 import { CARD_BACK_URI, TOKEN_CARD_BACK_URI } from "./config"
 
 function App() {
+
+  const backgroundImages = [
+    'https://www.desktophut.com/wp-content/uploads/2018/10/mtg-core-2019.jpg',
+    'https://external-preview.redd.it/jkg8pUW3tb-io-83uQU46VJLQlC92ZAvEpbPlUDU-co.jpg?auto=webp&s=822151ad423474960358232e132a9f91d2ccef60',
+    'https://images8.alphacoders.com/943/943285.jpg',
+    'https://wallpapercave.com/wp/wp4579177.jpg',
+    "https://media.magic.wizards.com/images/wallpaper/regal-caracal_eld_2560x1600_wallpaper.jpg",
+    'https://cdn.hipwallpaper.com/i/65/43/ibXTkY.jpg'
+  ]
+
   const [cards, setCards] = useState({})
   const [packCount, setPackCount] = useState(12)
   const [draftPacks, setDraftPacks] = useState("")
+  const [imageIndex, setImageIntex] = useState(Math.floor(Math.random()*backgroundImages.length))
 
-  function updateShowcase({cards, tokens=[], promos=[], masterpieces=[]}){
-      const lands = cards.filter(card => card.rarity === "common" && card.type_line.includes('Land'))
-      const commons = cards.filter(card => card.rarity === "common" && !card.type_line.includes('Land'));
-      const uncommons = cards.filter(card => card.rarity === "uncommon");
-      const rares = cards.filter(card => card.rarity === "rare");
-      const mythics = cards.filter(card => card.rarity === "mythic");  
-    setCards({ tokens, promos,lands, commons, uncommons, rares, mythics, masterpieces })
+  function updateShowcase({ cards, tokens = [], promos = [], masterpieces = [] }) {
+    const lands = cards.filter(card => card.rarity === "common" && card.type_line.includes('Land'))
+    const commons = cards.filter(card => card.rarity === "common" && !card.type_line.includes('Land'));
+    const uncommons = cards.filter(card => card.rarity === "uncommon");
+    const rares = cards.filter(card => card.rarity === "rare");
+    const mythics = cards.filter(card => card.rarity === "mythic");
+    setCards({ tokens, promos, lands, commons, uncommons, rares, mythics, masterpieces })
   }
 
-  useEffect(()=>{
+  useEffect(() => {
 
-    function makePack(){
+    function makePack() {
       const pack = [];
       // 16 cards
       // basic land, promo replacing foil, or masterpiece
@@ -57,7 +68,7 @@ function App() {
       for (let i = 0; i < 3; i++) {
         const index = Math.floor(Math.random() * cardData.uncommons.length)
         pack.push(cardData.uncommons[index])
-        cardData.uncommons.splice(index,1)
+        cardData.uncommons.splice(index, 1)
       }
       for (let i = 0; i < 10; i++) {
         const index = Math.floor(Math.random() * cardData.commons.length)
@@ -70,15 +81,15 @@ function App() {
       return pack
     }
 
-    function generateCustomDeckObject(deckIndex, pack){
+    function generateCustomDeckObject(deckIndex, pack) {
       const result = {}
 
       pack.forEach((card, cardIndex) => {
-        
-        const cardBack = card.set_type === "token" || card.layout === "emblem" ? TOKEN_CARD_BACK_URI:CARD_BACK_URI
-        
+
+        const cardBack = card.set_type === "token" || card.layout === "emblem" ? TOKEN_CARD_BACK_URI : CARD_BACK_URI
+
         result[`${1 + deckIndex}${cardIndex}`] = {
-          FaceURL: card.image_uris? card.image_uris.png: card.card_faces[0].image_uris.png,
+          FaceURL: card.image_uris ? card.image_uris.png : card.card_faces[0].image_uris.png,
           BackURL: card.image_uris ? cardBack : card.card_faces[1].image_uris.png,
           NumWidth: 1,
           NumHeight: 1,
@@ -89,7 +100,7 @@ function App() {
       return result
     }
 
-    function formatForTTS(packs){
+    function formatForTTS(packs) {
       const formattedPacks = {
         SaveName: "",
         GameMode: "",
@@ -106,7 +117,7 @@ function App() {
         ObjectStates: packs.map((pack, deckIndex) => ({
           Name: "Deck",
           Transform: {
-            posX: -4*calculatePosition(deckIndex, 'X'),
+            posX: -4 * calculatePosition(deckIndex, 'X'),
             posY: 1,
             posZ: -4 * calculatePosition(deckIndex, "Z"),
             rotX: 0,
@@ -135,69 +146,70 @@ function App() {
           HideWhenFaceDown: true,
           Hands: false,
           SidewaysCard: false,
-          DeckIDs:pack.map((card,cardIndex) => Number(`${1+deckIndex}${cardIndex}00`)),
+          DeckIDs: pack.map((card, cardIndex) => Number(`${1 + deckIndex}${cardIndex}00`)),
 
           CustomDeck: generateCustomDeckObject(deckIndex, pack),
           XmlUI: "",
           LuaScript: "",
           LuaScriptState: "",
           ContainedObjects: pack.map((card, cardIndex) => {
-            const cardBack = card.set_type === "token" || card.layout === "emblem"  ? TOKEN_CARD_BACK_URI : CARD_BACK_URI
+            const cardBack = card.set_type === "token" || card.layout === "emblem" ? TOKEN_CARD_BACK_URI : CARD_BACK_URI
             let cardName = card.name
-            if(card.set_type === "promo"){
-              cardName+= " - Promo";
+            if (card.set_type === "promo") {
+              cardName += " - Promo";
             }
             if (card.set_type === "masterpiece") {
               cardName += " - Masterpiece";
             }
             return ({
-            Name:"CardCustom",
-            Transform: {
-              posX: 0,
-              posY: 0,
-              posZ: 0,
-              rotX: 0,
-              rotY: 180,
-              rotZ: 180,
-              scaleX: 1,
-              scaleY: 1,
-              scaleZ: 1,
-            },
-            Nickname: cardName,
-            Description: `$${card.prices.usd || card.prices.usd_foil || 0}`,
-            GMNotes: "",
-            ColorDiffuse: {
-              r: 0.713235259,
-              g: 0.713235259,
-              b: 0.713235259
-            },
-            Locked: false,
-            Grid: true,
-            Snap: true,
-            IgnoreFoW: false,
-            Autoraise: true,
-            Sticky: true,
-            Tooltip: true,
-            GridProjection: false,
-            HideWhenFaceDown: true,
-            Hands: true,
-            SidewaysCard: false,
-            CardID: Number(`${1 + deckIndex}${cardIndex}00`),
-            CustomDeck:{
-              [`${1 + deckIndex}${cardIndex}`]:{
-                FaceURL: card.image_uris ? card.image_uris.png : card.card_faces[0].image_uris.png,
-                BackURL: card.image_uris ? cardBack : card.card_faces[1].image_uris.png,
-                NumWidth: 1,
-                NumHeight: 1,
-                BackIsHidden: true,
-                UniqueBack: false
-              }
-            },
-            XmlUI: "",
-            LuaScript: "",
-            LuaScriptState: "",
-          
-          })}
+              Name: "CardCustom",
+              Transform: {
+                posX: 0,
+                posY: 0,
+                posZ: 0,
+                rotX: 0,
+                rotY: 180,
+                rotZ: 180,
+                scaleX: 1,
+                scaleY: 1,
+                scaleZ: 1,
+              },
+              Nickname: cardName,
+              Description: `$${card.prices.usd || card.prices.usd_foil || 0}`,
+              GMNotes: "",
+              ColorDiffuse: {
+                r: 0.713235259,
+                g: 0.713235259,
+                b: 0.713235259
+              },
+              Locked: false,
+              Grid: true,
+              Snap: true,
+              IgnoreFoW: false,
+              Autoraise: true,
+              Sticky: true,
+              Tooltip: true,
+              GridProjection: false,
+              HideWhenFaceDown: true,
+              Hands: true,
+              SidewaysCard: false,
+              CardID: Number(`${1 + deckIndex}${cardIndex}00`),
+              CustomDeck: {
+                [`${1 + deckIndex}${cardIndex}`]: {
+                  FaceURL: card.image_uris ? card.image_uris.png : card.card_faces[0].image_uris.png,
+                  BackURL: card.image_uris ? cardBack : card.card_faces[1].image_uris.png,
+                  NumWidth: 1,
+                  NumHeight: 1,
+                  BackIsHidden: true,
+                  UniqueBack: false
+                }
+              },
+              XmlUI: "",
+              LuaScript: "",
+              LuaScriptState: "",
+
+            })
+          }
           )
 
         }))
@@ -206,14 +218,14 @@ function App() {
       return formattedPacks;
 
     }
-    
-    if(cards.commons && cards.commons.length > 0){
+
+    if (cards.commons && cards.commons.length > 0) {
       const packs = []
       for (let i = 0; i < packCount; i++) {
         packs.push(makePack())
       }
-      
-      
+
+
 
 
       setDraftPacks(JSON.stringify(formatForTTS(packs)))
@@ -221,20 +233,20 @@ function App() {
 
   }, [cards])
 
-  useEffect(()=>{
+  useEffect(() => {
 
-  },[draftPacks])
+  }, [draftPacks])
 
-  function updatePackCount(newCount){
+  function updatePackCount(newCount) {
     setPackCount(newCount)
   }
 
-  function calculatePosition(index, coordinate){
-    if(coordinate === 'X'){
+  function calculatePosition(index, coordinate) {
+    if (coordinate === 'X') {
       return index % 6
     }
     if (coordinate === 'Z') {
-      return Math.floor(index/6)
+      return Math.floor(index / 6)
     }
   }
 
@@ -256,21 +268,24 @@ function App() {
 
   return (
     <div className="App">
-      <StickyNav packCount={packCount} updatePackCount={updatePackCount}/>
+      <StickyNav packCount={packCount} updatePackCount={updatePackCount} />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="welcome-text">
-          Booster pack generator for <a className="tabletop-link" href="https://www.tabletopsimulator.com/" target="_blank" rel="noopener noreferrer">tabletop simulator</a>
-        </p>
+
+        <div className="cover-image" style={{backgroundImage: `url(${backgroundImages[imageIndex]})`}}>
+          <h1 className="header-text">Magic the gathering</h1>
+          <p className="welcome-text">
+            Booster pack generator for <a className="tabletop-link" href="https://www.tabletopsimulator.com/" target="_blank" rel="noopener noreferrer">tabletop simulator</a>
+          </p>
         <div className="controls">
 
-        <SetList onSearch={updateShowcase}/>
-        {draftPacks && <button class="download-button" onClick={download}> Download </button>}
+          <SetList onSearch={updateShowcase} />
+          {draftPacks && <button class="download-button" onClick={download}> Download </button>}
+        </div>
         </div>
         {draftPacks && <p className="showcase-intro">
-          Here is what you can expect to find in these packs: 
+          Here is what you can expect to find in these packs:
         </p>}
-        <Showcase cards={cards} /> 
+        <Showcase cards={cards} />
       </header>
       <Footer>
         <span>This booster pack generator is unofficial Fan Content permitted under the Fan Content Policy. Not approved/endorsed by Wizards. Portions of the materials used are property of Wizards of the Coast. ©Wizards of the Coast LLC</span>
